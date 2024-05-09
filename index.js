fd8f38d69da266d91fe9da374233e6ad6be7addb
Nikolai <niklonberg@gmail.com>
2024-05-09 15:37:50 +0200

    minor edits

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// const genRandomNum = require("./commonjs-utils-example"); // default CommonJS style import of a singular import.
+// this file, index.js isthe usual name for the entry point of a nodejs application.
+// This file is also sometime called app.js or server.js
+
+// const genRandomNum = require("./commonjs-utils-example"); // <-- default CommonJS style import of a singular import.
 // we can import functions, objects, arrays etc.
 // console.log(`random number is ${genRandomNum()}`);
 
-//in order to import multiple things, we change what we did above on line 1 to this:
+//in order to import multiple things, we change what we did above on to this:
 const {
   genRandomNum,
   celsiusToFahrenheit,
