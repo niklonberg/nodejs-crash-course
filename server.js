1beb9a5bea304add641019a94af9f75722c4104f
Nikolai <niklonberg@gmail.com>
2024-05-09 17:12:50 +0200

    edit server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //this will be the new entry point for this application
 import http from "http"; // <-- this import looks a little different, because we are importing something that is included with nodejs
 // const PORT = 8000; // <-- we will use process.env variables for this later
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 //create server
 // const server = http.createServer((req, res)=> { // we use the createServer method which takes a function, (does not need to be an arrow function)
@@ -10,18 +10,36 @@ const PORT = process.env.PORT
 //   res.end();
 // })
 
-const server = http.createServer((req, res) => {
-  // we can also set headers, such as its content type.
-  // content-type specifies the type of content being sent in the http response or request body
-  // commonly text/html for HTML content, application/json for JSON data, or image/jpeg for JPEG images. See below
-  // res.setHeader("Content-Type", "text/html");
-  // res.statusCode = 404; // we can set status codes too
-  // the above can be done in one line using the writeHead method
-  // res.end("<h1>Hello, World!</h1>");
-  res.writeHead(200, { "Content-Type": "text/html" });
+//create server
+// const server = http.createServer((req, res) => {
+//   // we can also set headers, such as its content type.
+//   // content-type specifies the type of content being sent in the http response or request body
+//   // commonly text/html for HTML content, application/json for JSON data, or image/jpeg for JPEG images. See below
+//   // res.setHeader("Content-Type", "text/html");
+//   // res.statusCode = 404; // we can set status codes too
+//   // the above can be done in one line using the writeHead method
+//   // res.end("<h1>Hello, World!</h1>");
+//   res.writeHead(200, { "Content-Type": "text/html" });
 
+//   res.end("<h1>Hello</h1>");
+// });
+
+//create server
+const server = http.createServer((req, res) => {
+  // we can also use the req object, lets log it
+  console.log(req.url); // logs '/' if on http://localhost:8087/
+  console.log(req.method); // logs GET
+
+  // unfortunately, no matter what method or url we use, we are giving the same response back
+  // when you dont use a framework like express, you need to manually check the url
+  // in express you could do 
+  // app.post('/users', () => {}) whatever we put inside the function would only run when we made a post request to /users
+
+  res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Hello</h1>");
 });
+
+
 
 // as it stands, this server variable isnt doing much, we need to listen to it.
 server.listen(PORT, () => {
