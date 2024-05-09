e75fef07d85fa4f60faf70865f5030bf77b3598c
Nikolai <niklonberg@gmail.com>
2024-05-09 15:32:47 +0200

    add example of multiple export/imports in commonjs

--- commonjs-utils-example.js
+++ commonjs-utils-example.js
@@ -7,4 +7,16 @@ nodejs uses the CommonJS module system by default
 in order to export this function and use it always,
 we need to do the following:
 */
-module.exports = genRandomNum;
+// module.exports = genRandomNum;
+
+//lets say we add another function.
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+//in order to export both of these, we need to change our module.exports statement above to this:
+module.exports = {
+  genRandomNum,
+  celsiusToFahrenheit,
+};
+// we wrap what we want to export into an object
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const genRandomNum = require("./commonjs-utils-example"); // default CommonJS style import.
+// const genRandomNum = require("./commonjs-utils-example"); // default CommonJS style import of a singular import.
 // we can import functions, objects, arrays etc.
+// console.log(`random number is ${genRandomNum()}`);
+
+//in order to import multiple things, we change what we did above on line 1 to this:
+const {
+  genRandomNum,
+  celsiusToFahrenheit,
+} = require("./commonjs-utils-example"); //use object destructuring
+
 console.log(`random number is ${genRandomNum()}`);
+console.log(`0 Celsius to fahrenheit: ${celsiusToFahrenheit(0)}`);
