70a9f1413a3bd1bff17d8ce27cd0fce7c7db0184
Nikolai <niklonberg@gmail.com>
2024-05-09 15:27:06 +0200

    add import of genRandomNum

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("hello world");
+const genRandomNum = require("./commonjs-utils-example"); // default CommonJS style import.
+// we can import functions, objects, arrays etc.
+console.log(`random number is ${genRandomNum()}`);
